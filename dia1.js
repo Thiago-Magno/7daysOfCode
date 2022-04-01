b5490b886fceb45d47342530eb3537b2506781f2
Thiago-Magno <thiagomagnoom@gmail.com>
2022-04-01 17:44:24 -0300

    resolução corrigida

--- dia1.js
+++ dia1.js
@@ -4,7 +4,7 @@ let stringUm = '1'
 let numeroTrinta = 30
 let stringTrinta = '30'
 let numeroDez = 10
-let stringDez = '11'
+let stringDez = '10'
 
 if (numeroUm == stringUm) {
   console.log('As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes')
@@ -12,7 +12,7 @@ if (numeroUm == stringUm) {
   console.log('As variáveis numeroUm e stringUm não tem o mesmo valor')
 }
 
- if (numeroTrinta == stringTrinta) {
+ if (numeroTrinta === stringTrinta) {
    console.log('As variáveis numeroTrinta e stringTrinta tem o mesmo valor e mesmo tipo')
  } else {
    console.log('As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo')
